a99b7ef6e40ee43b696ccd947105a5c64f085d62
MYKOLL DANIEL CUNHA GUIMARAES <mykoll.dcguimaraes@senacsp.edu.br>
2025-02-15 11:52:18 -0300

    componentes

--- teste/src/App.jsx
+++ teste/src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import Prova from './components/Prova'
+import Saudacao from './components/Saudacao'
+import Preco from './components/Preco'
 function App() {
-
+    
   return (
     <>
       <h1>Teste</h1>
       <p>Olá mundo</p>
       <Prova/>
+      <Saudacao nome="Pedro" idade={10+10} curso="React"/>
+      <Preco/>
     </>
   )
 }
---
+++ teste/src/components/Saudacao.jsx
@@ -0,0 +1,12 @@
+
+
+function Saudacao(props) {
+
+  return (
+    <div>
+        Olá {props.nome}!, tenho {props.idade} no curso {props.curso}
+    </div>
+  )
+}
+
+export default Saudacao;
